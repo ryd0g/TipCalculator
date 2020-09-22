b32faa364395cd0b8fc81734da9be0703d2efb78
Ryan Lee <rjlguitar@gmail.com>
2020-09-22 11:18:35 -0700

    added js function for calc

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const tip = document.querySelector('#display-tip')
+const total = document.querySelector('#display-total')
+const per = document.querySelector('#display-per')
+const billInput = document.querySelector('#input-bill')
+const tipInput = document.querySelector('#input-tip')
+const peopleInput = document.querySelector('#input-people')
+
+function calculateTip() {
+    const billValue = billInput.value
+    const tipValue = tipInput.value
+    const peopleValue = peopleInput.value
+
+    const tipAmount = billValue * tipValue / 100
+    const totalAmount = parseFloat(billValue + tipAmount)
+    const perAmount = parseFloat(tipAmount / peopleValue)
+  
+    tip.innerHTML = '$' + tipAmount.toFixed(2)
+    total.innerHTML = '$' + totalAmount.toFixed(2)
+    per.innerHTML = '$' + perAmount.toFixed(2)
+  }
+

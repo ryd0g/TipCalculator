300993c708fdac7f7a0955a4e16030f198107151
Ryan Lee <rjlguitar@gmail.com>
2020-09-22 23:00:17 -0700

    added amount per person

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const tip = document.querySelector('#display-tip')
 const total = document.querySelector('#display-total')
-const per = document.querySelector('#display-per')
+const perTip = document.querySelector('#display-per')
+const perAmount = document.querySelector('#display-each')
 const billInput = document.querySelector('#input-bill')
 const tipInput = document.querySelector('#input-tip')
 const peopleInput = document.querySelector('#input-people')
@@ -11,12 +12,14 @@ function calculateTip() {
     const peopleValue = peopleInput.value
 
     const tipAmount = billValue * tipValue / 100
-    const totalAmount = parseFloat(tipAmount) + parseFloat(billValue)
-    const perAmount = parseFloat(tipAmount / peopleValue)
+    const tipEach = tipAmount / peopleValue
+    const totalAmount = parseFloat(billValue) + parseFloat(tipAmount)
+    const totalPer = totalAmount / peopleValue
   
     tip.innerHTML = '$' + tipAmount.toFixed(2)
+    perTip.innerHTML = '$' + tipEach.toFixed(2)
     total.innerHTML = '$' + totalAmount.toFixed(2)
-    per.innerHTML = '$' + perAmount.toFixed(2)
+    perAmount.innerHTML = '$' + totalPer.toFixed(2)
   }
 
 billInput.addEventListener('input', calculateTip)
